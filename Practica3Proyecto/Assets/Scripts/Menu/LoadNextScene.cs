ff3f387d12496c90417552d9cf6d6e5fccfc977a
Cyandic35 <oku400@gmail.com>
2022-11-14 20:10:17 +0100

    14-11-2022

--- Practica3Proyecto/Assets/Scripts/Menu/LoadNextScene.cs
+++ Practica3Proyecto/Assets/Scripts/Menu/LoadNextScene.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LoadNextScene : MonoBehaviour
 {
-    public void Load()
+    public void BeginGame()
     {
         int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
 
-        if (nextScene > SceneManager.sceneCountInBuildSettings)
+        if (nextScene >= SceneManager.sceneCountInBuildSettings)
         {
             nextScene = 0;
         }
+        Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene(nextScene);
     }
 }
