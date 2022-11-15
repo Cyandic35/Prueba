4dfb5c4c869ffcf1c2fefe7e31447a90aea338a7
Cyandic35 <oku400@gmail.com>
2022-11-15 19:17:30 +0100

    15-11-2022

--- My project prueba/Assets/Ejercicios Scripts/27-10-2022/EnemyHealth.cs
+++ My project prueba/Assets/Ejercicios Scripts/27-10-2022/EnemyHealth.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyHealth : Health
 {
+    private Animator myAnimator;
+    public float timeToDie = 4;
+    private NavMeshAgent agent;
+    private Collider myCollider;
+
+    private void Awake()
+    {
+        myCollider = GetComponent<Collider>();
+        agent = GetComponent<NavMeshAgent>();
+        myAnimator = GetComponentInChildren<Animator>();
+    }
+
     public override void Die()
     {
+        myCollider.enabled = false;
+        myAnimator.SetTrigger("Ded");
+        agent.isStopped = true;
+        StartCoroutine(DedCoroutine());
+    }
+
+    public IEnumerator DedCoroutine()
+    {
+        yield return new WaitForSeconds(timeToDie);
         Destroy(gameObject);
     }
 }
